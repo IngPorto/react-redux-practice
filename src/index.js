c41d83a91965a56f3c26b0f74d871ad30f349cca
David Portocarrero Caicedo <ing.portocarrero@gmail.com>
2019-02-26 19:25:37 -0500

    Initial commit from remote

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react'
+import {render} from 'react-dom'
+import Main from './containers/main'
+
+render(
+    <Main />,
+    document.getElementById('main')
+    )
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    entry: __dirname + '/src/index.js',
+    output: {
+        filename: 'main.js',
+        path: __dirname + '/public/dist'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env','@babel/preset-react']
+                }
+                }
+            }
+        ]
+    }
+}
