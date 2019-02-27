9ffc9a8a5a6387b6f2ebfbd0934c4fbde6d408be
David Portocarrero Caicedo <ing.portocarrero@gmail.com>
2019-02-26 22:53:27 -0500

    [upload] running redux and webpack

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,10 +1,51 @@
 import React, {Component} from 'react'
+import {createStore} from 'redux'
+
+const initalState =[
+        {
+            "titulo": "United State"
+        },
+        {
+            "titulo": "Colombia"
+        },
+        {
+            "titulo": "Cuba"
+        },
+        {
+            "titulo": "Brasil"
+        }
+    ]
 
 export default class Main extends Component{
+    
+
+    constructor(props){
+        super(props)
+        const store = createStore(
+            (state) => state,
+            initalState,
+            window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        )
+    }
+    componentDidMount(){
+        const $form = document.getElementById('busqueda')
+        $form.addEventListener("submit", this.handleSubmit)
+    }
+
+    handleSubmit = event =>{
+        const $form = document.getElementById('busqueda')
+        event.preventDefault();
+        const objetoForm = new FormData($form)
+        const titulo = objetoForm.get('titulo')
+        console.log(titulo)
+    }
+
     render(){
         return(
             <div className="Main">
-                
+                <form id="busqueda">
+                    <input type="text" name="titulo" placeholder="Búscar" />
+                </form>
             </div>
         )
     }
