0a6a99ae58085cc3e175a61c8c1d60347fa6f594
David Portocarrero Caicedo <ing.portocarrero@gmail.com>
2019-02-28 21:08:25 -0500

    [add] separates the data of the logic

---
+++ src/api/data.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+        "titulo": "United State"
+    },
+    {
+        "titulo": "Colombia"
+    },
+    {
+        "titulo": "Cuba"
+    },
+    {
+        "titulo": "Brasil"
+    }
+]
